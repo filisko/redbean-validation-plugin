52ba52c8d1c25c3d69915d953179b7b258f17cb7
Filisko <filisfutsarov@gmail.com>
2016-04-19 17:33:05 +0200

    BeanModelValidationPlugin_Exception -> ModelValidationPlugin_Exception

--- src/ModelValidation.php
+++ src/ModelValidation.php
@@ -7,19 +7,19 @@ class ModelValidation implements \RedBeanPHP\Plugin
     {
         $data = $bean->export();
         $model = $bean->box() !== null ? $bean->box() : null;
-        
+
         if ( ! $model )
         {
-            throw new BeanModelValidationPlugin_Exception('This bean does not have a model!');
+            throw new ModelValidationPlugin_Exception('This bean does not have a model!');
         }
-        
+
         $rules = isset($model::$rules) ? $model::$rules : null;
-        
+
         if ( ! $rules )
         {
-            throw new BeanModelValidationPlugin_Exception('This bean does not have any established rules!');
+            throw new ModelValidationPlugin_Exception('This bean does not have any established rules!');
         }
-        
+
         $validations = [];
         $filters = [];
         $labels = [];
@@ -31,12 +31,12 @@ class ModelValidation implements \RedBeanPHP\Plugin
             {
                 $filters[$field] = $rule['filter'];
             }
-            
+
             if (isset($rule['label']))
             {
                 $labels[$field] = $rule['label'];
             }
-            
+
             if (isset($rule['validation']))
             {
                 $validations[$field] = $rule['validation'];
@@ -50,35 +50,35 @@ class ModelValidation implements \RedBeanPHP\Plugin
         }
 
         $gump = new \GUMP();
-        
+
         if ( ! empty($filters))
         {
             $gump->filter_rules($filters);
         }
-        
+
         if ( ! empty($validations))
         {
             $gump->validation_rules($validations);
         }
-        
+
         if ( ! empty($labels))
         {
             $gump->set_field_names($labels);
         }
-        
+
         $validated_data = $gump->run($data);
 
         if($validated_data === false)
         {
             return self::default2custom_errors($gump->get_errors_array(), $messages);
-        } 
+        }
         else
         {
             $bean->import($validated_data);
-            return true;  
+            return true;
         }
     }
-    
+
     private static function default2custom_errors($default, $custom)
     {
         // Replace default error messages with our custom ones
@@ -90,4 +90,4 @@ class ModelValidation implements \RedBeanPHP\Plugin
         return $errors;
     }
 }
-class BeanModelValidationPlugin_Exception extends \Exception {}
+class ModelValidationPlugin_Exception extends \Exception {}
